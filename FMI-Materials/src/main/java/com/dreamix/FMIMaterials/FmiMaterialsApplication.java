0830d18b5a99e78ef482ade7a6a97c214015dc30
Mario Nikolov <56367284+brokenhalo420@users.noreply.github.com>
2022-05-21 22:58:09 +0300

    First commit with Spring Boot project (#2)
    Co-authored-by: Mario Nikolov <m.nikolov1501@gmail.com>

---
+++ FMI-Materials/src/main/java/com/dreamix/FMIMaterials/FmiMaterialsApplication.java
@@ -0,0 +1,20 @@
+package com.dreamix.FMIMaterials;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmiMaterialsApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FmiMaterialsApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
