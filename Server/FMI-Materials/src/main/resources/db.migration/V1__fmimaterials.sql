dc87dd06da1b911be6f24e8e5fa8fcc01040e04a
Georgi Boychev <georgiboychevv@gmail.com>
2022-06-26 01:15:24 +0300

    very important sql changes

--- Server/FMI-Materials/src/main/resources/db.migration/V1__fmimaterials.sql
+++ Server/FMI-Materials/src/main/resources/db.migration/V1__fmimaterials.sql
@@ -1,40 +1,202 @@
-USE fmimaterials;
+CREATE DATABASE  IF NOT EXISTS `library` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `library`;
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: library
+-- ------------------------------------------------------
+-- Server version	8.0.17
 
-CREATE TABLE user
-(
-    id         int         NOT NULL PRIMARY KEY,
-    fullname   VARCHAR(80) NOT NULL,
-    email      VARCHAR(80) NOT NULL,
-    passwd     VARCHAR(80) NOT NULL,
-    materialid VARCHAR(36) NOT NULL UNIQUE KEY
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `course_material`
+--
 
-CREATE TABLE course
-(
-    id         int         NOT NULL PRIMARY KEY,
-    fullname   VARCHAR(80) NOT NULL,
-    materialid VARCHAR(36) NOT NULL UNIQUE KEY
-);
+DROP TABLE IF EXISTS `course_material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `course_material` (
+                                   `course_id` bigint(20) NOT NULL,
+                                   `material_id` bigint(20) NOT NULL,
+                                   PRIMARY KEY (`course_id`,`material_id`),
+                                   KEY `FKhmsl5aah2ytr5yjqqi1jnx2db` (`material_id`),
+                                   CONSTRAINT `FKhmsl5aah2ytr5yjqqi1jnx2db` FOREIGN KEY (`material_id`) REFERENCES `materials` (`id`),
+                                   CONSTRAINT `FKmq2qrxp40egs2kfrecuex4vbq` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE material
-(
-    id            int         NOT NULL PRIMARY KEY,
-    fullname      VARCHAR(80) NOT NULL,
-    users         VARCHAR(36) NOT NULL UNIQUE KEY,
-    material_type VARCHAR(80) NOT NULL UNIQUE KEY,
-    courses       VARCHAR(36) NOT NULL UNIQUE KEY
-);
+--
+-- Dumping data for table `course_material`
+--
 
-CREATE TABLE group
-(
-    id      int         NOT NULL PRIMARY KEY,
-    courses VARCHAR(36) NOT NULL UNIQUE KEY
-);
+LOCK TABLES `course_material` WRITE;
+/*!40000 ALTER TABLE `course_material` DISABLE KEYS */;
+/*!40000 ALTER TABLE `course_material` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE material_type
-(
-    id       int         NOT NULL PRIMARY KEY,
-    fullname VARCHAR(80) NOT NULL,
-    courses  VARCHAR(36) NOT NULL UNIQUE KEY
-);
+--
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `courses` (
+                           `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                           `course_group` varchar(20) NOT NULL,
+                           `fullname` varchar(40) NOT NULL,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `courses`
+--
+
+LOCK TABLES `courses` WRITE;
+/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hibernate_sequence`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hibernate_sequence` (
+    `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+LOCK TABLES `hibernate_sequence` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` VALUES (1);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `material_types`
+--
+
+DROP TABLE IF EXISTS `material_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `material_types` (
+                                  `id` bigint(20) NOT NULL,
+                                  `fullname` varchar(40) NOT NULL,
+                                  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `material_types`
+--
+
+LOCK TABLES `material_types` WRITE;
+/*!40000 ALTER TABLE `material_types` DISABLE KEYS */;
+/*!40000 ALTER TABLE `material_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materials`
+--
+
+DROP TABLE IF EXISTS `materials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materials` (
+                             `id` bigint(20) NOT NULL,
+                             `fullname` varchar(40) NOT NULL,
+                             `material_type_id` bigint(20) DEFAULT NULL,
+                             PRIMARY KEY (`id`),
+                             KEY `FKjympeb8d1yw5t8kcn1ldn1l7h` (`material_type_id`),
+                             CONSTRAINT `FKjympeb8d1yw5t8kcn1ldn1l7h` FOREIGN KEY (`material_type_id`) REFERENCES `material_types` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materials`
+--
+
+LOCK TABLES `materials` WRITE;
+/*!40000 ALTER TABLE `materials` DISABLE KEYS */;
+/*!40000 ALTER TABLE `materials` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_material`
+--
+
+DROP TABLE IF EXISTS `user_material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_material` (
+                                 `user_id` bigint(20) NOT NULL,
+                                 `material_id` bigint(20) NOT NULL,
+                                 PRIMARY KEY (`user_id`,`material_id`),
+                                 KEY `FKl88nkc4uuii5hpreq63tm1soe` (`material_id`),
+                                 CONSTRAINT `FK9r915mw3hk2ph94i2ui1kbqhq` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+                                 CONSTRAINT `FKl88nkc4uuii5hpreq63tm1soe` FOREIGN KEY (`material_id`) REFERENCES `materials` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_material`
+--
+
+LOCK TABLES `user_material` WRITE;
+/*!40000 ALTER TABLE `user_material` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_material` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+                         `id` bigint(20) NOT NULL,
+                         `email` varchar(40) NOT NULL,
+                         `fullname` varchar(40) NOT NULL,
+                         `passwd` varchar(40) NOT NULL,
+                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'test@gmail.com','joro','parola1'),(2,'pe6o@gmail.com','pe6ogo6o','parolaPe6o'),(3,'sandokan@gmail.com','sando_mando','parolaSando');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-26  0:40:55
