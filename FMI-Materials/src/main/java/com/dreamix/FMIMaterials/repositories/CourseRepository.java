b290ad989337308fac07ebd9443a07016510ddec
Mario Nikolov <m.nikolov1501@gmail.com>
2022-06-25 21:07:46 +0300

    changes to project

--- FMI-Materials/src/main/java/com/dreamix/FMIMaterials/repositories/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dreamix.FMIMaterials.repositories;
-
-import com.dreamix.FMIMaterials.models.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
